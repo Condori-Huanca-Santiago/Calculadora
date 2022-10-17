0c21a933b720cd544e4a10180c8ca95b77d6511f
Condori-Huanca-Santiago <83928656+Condori-Huanca-Santiago@users.noreply.github.com>
2022-10-17 19:03:40 -0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CalcMaiacondori
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.uno = New System.Windows.Forms.Button()
+        Me.tres = New System.Windows.Forms.Button()
+        Me.siete = New System.Windows.Forms.Button()
+        Me.Cero = New System.Windows.Forms.Button()
+        Me.dos = New System.Windows.Forms.Button()
+        Me.cinco = New System.Windows.Forms.Button()
+        Me.seis = New System.Windows.Forms.Button()
+        Me.cuatro = New System.Windows.Forms.Button()
+        Me.ocho = New System.Windows.Forms.Button()
+        Me.nueve = New System.Windows.Forms.Button()
+        Me.mas = New System.Windows.Forms.Button()
+        Me.menos = New System.Windows.Forms.Button()
+        Me.Multi = New System.Windows.Forms.Button()
+        Me.Divi = New System.Windows.Forms.Button()
+        Me.Numeros1 = New System.Windows.Forms.Label()
+        Me.Borrar = New System.Windows.Forms.Button()
+        Me.Punto = New System.Windows.Forms.Button()
+        Me.Igual = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'uno
+        '
+        Me.uno.Location = New System.Drawing.Point(21, 191)
+        Me.uno.Name = "uno"
+        Me.uno.Size = New System.Drawing.Size(75, 23)
+        Me.uno.TabIndex = 0
+        Me.uno.Text = "1"
+        Me.uno.UseVisualStyleBackColor = True
+        '
+        'tres
+        '
+        Me.tres.Location = New System.Drawing.Point(183, 191)
+        Me.tres.Name = "tres"
+        Me.tres.Size = New System.Drawing.Size(75, 23)
+        Me.tres.TabIndex = 1
+        Me.tres.Text = "3"
+        Me.tres.UseVisualStyleBackColor = True
+        '
+        'siete
+        '
+        Me.siete.Location = New System.Drawing.Point(21, 122)
+        Me.siete.Name = "siete"
+        Me.siete.Size = New System.Drawing.Size(75, 23)
+        Me.siete.TabIndex = 2
+        Me.siete.Text = "7"
+        Me.siete.UseVisualStyleBackColor = True
+        '
+        'Cero
+        '
+        Me.Cero.Location = New System.Drawing.Point(102, 229)
+        Me.Cero.Name = "Cero"
+        Me.Cero.Size = New System.Drawing.Size(75, 23)
+        Me.Cero.TabIndex = 3
+        Me.Cero.Text = "0"
+        Me.Cero.UseVisualStyleBackColor = True
+        '
+        'dos
+        '
+        Me.dos.Location = New System.Drawing.Point(102, 191)
+        Me.dos.Name = "dos"
+        Me.dos.Size = New System.Drawing.Size(75, 23)
+        Me.dos.TabIndex = 4
+        Me.dos.Text = "2"
+        Me.dos.UseVisualStyleBackColor = True
+        '
+        'cinco
+        '
+        Me.cinco.Location = New System.Drawing.Point(101, 155)
+        Me.cinco.Name = "cinco"
+        Me.cinco.Size = New System.Drawing.Size(75, 23)
+        Me.cinco.TabIndex = 5
+        Me.cinco.Text = "5"
+        Me.cinco.UseVisualStyleBackColor = True
+        '
+        'seis
+        '
+        Me.seis.Location = New System.Drawing.Point(183, 155)
+        Me.seis.Name = "seis"
+        Me.seis.Size = New System.Drawing.Size(75, 23)
+        Me.seis.TabIndex = 6
+        Me.seis.Text = "6"
+        Me.seis.UseVisualStyleBackColor = True
+        '
+        'cuatro
+        '
+        Me.cuatro.Location = New System.Drawing.Point(21, 155)
+        Me.cuatro.Name = "cuatro"
+        Me.cuatro.Size = New System.Drawing.Size(75, 23)
+        Me.cuatro.TabIndex = 7
+        Me.cuatro.Text = "4"
+        Me.cuatro.UseVisualStyleBackColor = True
+        '
+        'ocho
+        '
+        Me.ocho.Location = New System.Drawing.Point(102, 122)
+        Me.ocho.Name = "ocho"
+        Me.ocho.Size = New System.Drawing.Size(75, 23)
+        Me.ocho.TabIndex = 8
+        Me.ocho.Text = "8"
+        Me.ocho.UseVisualStyleBackColor = True
+        '
+        'nueve
+        '
+        Me.nueve.Location = New System.Drawing.Point(183, 122)
+        Me.nueve.Name = "nueve"
+        Me.nueve.Size = New System.Drawing.Size(75, 23)
+        Me.nueve.TabIndex = 9
+        Me.nueve.Text = "9"
+        Me.nueve.UseVisualStyleBackColor = True
+        '
+        'mas
+        '
+        Me.mas.Location = New System.Drawing.Point(21, 80)
+        Me.mas.Name = "mas"
+        Me.mas.Size = New System.Drawing.Size(36, 23)
+        Me.mas.TabIndex = 10
+        Me.mas.Text = "+"
+        Me.mas.UseVisualStyleBackColor = True
+        '
+        'menos
+        '
+        Me.menos.Location = New System.Drawing.Point(63, 80)
+        Me.menos.Name = "menos"
+        Me.menos.Size = New System.Drawing.Size(33, 23)
+        Me.menos.TabIndex = 11
+        Me.menos.Text = "-"
+        Me.menos.UseVisualStyleBackColor = True
+        '
+        'Multi
+        '
+        Me.Multi.Location = New System.Drawing.Point(102, 80)
+        Me.Multi.Name = "Multi"
+        Me.Multi.Size = New System.Drawing.Size(44, 23)
+        Me.Multi.TabIndex = 12
+        Me.Multi.Text = "×"
+        Me.Multi.UseVisualStyleBackColor = True
+        '
+        'Divi
+        '
+        Me.Divi.Location = New System.Drawing.Point(152, 80)
+        Me.Divi.Name = "Divi"
+        Me.Divi.Size = New System.Drawing.Size(25, 23)
+        Me.Divi.TabIndex = 13
+        Me.Divi.Text = "÷"
+        Me.Divi.UseVisualStyleBackColor = True
+        '
+        'Numeros1
+        '
+        Me.Numeros1.AutoSize = True
+        Me.Numeros1.Location = New System.Drawing.Point(18, 49)
+        Me.Numeros1.Name = "Numeros1"
+        Me.Numeros1.Size = New System.Drawing.Size(0, 13)
+        Me.Numeros1.TabIndex = 14
+        '
+        'Borrar
+        '
+        Me.Borrar.Location = New System.Drawing.Point(193, 80)
+        Me.Borrar.Name = "Borrar"
+        Me.Borrar.Size = New System.Drawing.Size(52, 23)
+        Me.Borrar.TabIndex = 16
+        Me.Borrar.Text = "C"
+        Me.Borrar.UseVisualStyleBackColor = True
+        '
+        'Punto
+        '
+        Me.Punto.Location = New System.Drawing.Point(183, 229)
+        Me.Punto.Name = "Punto"
+        Me.Punto.Size = New System.Drawing.Size(75, 23)
+        Me.Punto.TabIndex = 17
+        Me.Punto.Text = "."
+        Me.Punto.UseVisualStyleBackColor = True
+        '
+        'Igual
+        '
+        Me.Igual.Location = New System.Drawing.Point(21, 229)
+        Me.Igual.Name = "Igual"
+        Me.Igual.Size = New System.Drawing.Size(75, 23)
+        Me.Igual.TabIndex = 18
+        Me.Igual.Text = "="
+        Me.Igual.UseVisualStyleBackColor = True
+        '
+        'CalcMaiacondori
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Igual)
+        Me.Controls.Add(Me.Punto)
+        Me.Controls.Add(Me.Borrar)
+        Me.Controls.Add(Me.Numeros1)
+        Me.Controls.Add(Me.Divi)
+        Me.Controls.Add(Me.Multi)
+        Me.Controls.Add(Me.menos)
+        Me.Controls.Add(Me.mas)
+        Me.Controls.Add(Me.nueve)
+        Me.Controls.Add(Me.ocho)
+        Me.Controls.Add(Me.cuatro)
+        Me.Controls.Add(Me.seis)
+        Me.Controls.Add(Me.cinco)
+        Me.Controls.Add(Me.dos)
+        Me.Controls.Add(Me.Cero)
+        Me.Controls.Add(Me.siete)
+        Me.Controls.Add(Me.tres)
+        Me.Controls.Add(Me.uno)
+        Me.Name = "CalcMaiacondori"
+        Me.Text = "Calculadora simple"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents uno As System.Windows.Forms.Button
+    Friend WithEvents tres As System.Windows.Forms.Button
+    Friend WithEvents siete As System.Windows.Forms.Button
+    Friend WithEvents Cero As System.Windows.Forms.Button
+    Friend WithEvents dos As System.Windows.Forms.Button
+    Friend WithEvents cinco As System.Windows.Forms.Button
+    Friend WithEvents seis As System.Windows.Forms.Button
+    Friend WithEvents cuatro As System.Windows.Forms.Button
+    Friend WithEvents ocho As System.Windows.Forms.Button
+    Friend WithEvents nueve As System.Windows.Forms.Button
+    Friend WithEvents mas As System.Windows.Forms.Button
+    Friend WithEvents menos As System.Windows.Forms.Button
+    Friend WithEvents Multi As System.Windows.Forms.Button
+    Friend WithEvents Divi As System.Windows.Forms.Button
+    Friend WithEvents Numeros1 As System.Windows.Forms.Label
+    Friend WithEvents Borrar As System.Windows.Forms.Button
+    Friend WithEvents Punto As System.Windows.Forms.Button
+    Friend WithEvents Igual As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,131 @@
+﻿Public Class CalcMaiacondori
+    Dim operacion As String
+    Dim resultado As Nullable(Of Double) = Nothing
+    Dim V2 As Nullable(Of Double) = Nothing
+    Dim Pressopreador As Boolean
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub siete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles siete.Click
+        Rest()
+        Numeros1.Text &= "7"
+    End Sub
+
+    Private Sub ocho_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ocho.Click
+        Rest()
+        Numeros1.Text &= "8"
+    End Sub
+
+    Private Sub mas_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mas.Click
+        operacion = "+"
+        Buscasiope()
+    End Sub
+
+    Private Sub menos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles menos.Click
+
+        operacion = "-"
+        Buscasiope()
+    End Sub
+
+    Private Sub Multi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Multi.Click
+
+        operacion = "×"
+        Buscasiope()
+    End Sub
+
+    Private Sub Divi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Divi.Click
+
+        operacion = "÷"
+        Buscasiope()
+    End Sub
+
+    Private Sub nueve_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nueve.Click
+        Rest()
+        Numeros1.Text &= "9"
+    End Sub
+
+    Private Sub cuatro_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cuatro.Click
+        Rest()
+        Numeros1.Text &= "4"
+    End Sub
+
+    Private Sub cinco_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cinco.Click
+        Rest()
+        Numeros1.Text &= "5"
+    End Sub
+
+    Private Sub seis_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles seis.Click
+        Rest()
+        Numeros1.Text &= "6"
+    End Sub
+
+    Private Sub uno_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles uno.Click
+        Rest()
+        Numeros1.Text &= "1"
+    End Sub
+
+    Private Sub dos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dos.Click
+        Rest()
+        Numeros1.Text &= "2"
+    End Sub
+
+    Private Sub tres_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tres.Click
+        Rest()
+        Numeros1.Text &= "3"
+    End Sub
+
+    Private Sub Cero_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cero.Click
+        Rest()
+        Numeros1.Text &= "0"
+
+    End Sub
+
+    Private Sub Punto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Punto.Click
+        If InStr(Numeros1.Text, ".", CompareMethod.Text) = 0 Then
+            Numeros1.Text += "."
+        End If
+
+    End Sub
+    Private Sub Igual_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Igual.Click
+        Buscasiope()
+        operacion = ""
+    End Sub
+    Public Sub Buscasiope()
+        Pressopreador = True
+        V2 = Val(Numeros1.Text)
+        If resultado IsNot Nothing Then
+            Select Case operacion
+                Case "+"
+                    resultado = resultado + V2
+                Case "-"
+                    resultado -= V2
+                Case "×"
+                    resultado *= V2
+                Case "÷"
+                    resultado /= V2
+            End Select
+            Numeros1.Text = resultado
+        Else
+            resultado = V2
+
+        End If
+    End Sub
+    Public Sub Rest()
+        If Pressopreador = True Then
+            Numeros1.Text = ""
+            Pressopreador = False
+        ElseIf Numeros1.Text = "0" Then
+            Numeros1.Text = ""
+        End If
+    End Sub
+
+    Private Sub Borrar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Borrar.Click
+        Numeros1.Text = "0"
+        V2 = Nothing
+        resultado = Nothing
+    End Sub
+End Class
